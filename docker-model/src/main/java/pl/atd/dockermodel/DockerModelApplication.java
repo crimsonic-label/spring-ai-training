b481b2804f393353bff686fd5955cd4ed65cffb7
Andrzej D <adrozdow@o2.pl>
2025-08-28 21:09:13 +0200

    docker model module

---
+++ docker-model/src/main/java/pl/atd/dockermodel/DockerModelApplication.java
@@ -0,0 +1,12 @@
+package pl.atd.dockermodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerModelApplication.class, args);
+	}
+}
