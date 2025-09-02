d0056506c5dca4bbe9ba84ca4d996b35564f526e
Andrzej D <adrozdow@o2.pl>
2025-09-02 20:37:54 +0200

    multiple models

---
+++ multiple-models/src/main/java/pl/atd/multimodels/MultiModelsApplication.java
@@ -0,0 +1,12 @@
+package pl.atd.multimodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModelsApplication.class, args);
+	}
+}
