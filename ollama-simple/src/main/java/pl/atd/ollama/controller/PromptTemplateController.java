95d6c10cc32ae2bfb09f7c01748c644328e23005
Andrzej D <adrozdow@o2.pl>
2025-09-06 17:02:58 +0200

    prompt template

---
+++ ollama-simple/src/main/java/pl/atd/ollama/controller/PromptTemplateController.java
@@ -0,0 +1,35 @@
+package pl.atd.ollama.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PromptTemplateController {
+
+  private final ChatClient chatClient;
+
+  @Value("classpath:/promptTemplates/userPromptTemplate.st")
+  Resource userPromptTemplate;
+
+  @GetMapping("/email")
+  public String emailResponse(@RequestParam("customerName") String customerName,
+      @RequestParam("customerMessage") String customerMessage) {
+    return chatClient.prompt()
+        .system("""
+            You are a professional customer service assistant which helps drafting email
+            responses to improve the productivity of the customer support team
+            """)
+        .user(promptUserSpec -> promptUserSpec.text(userPromptTemplate)
+            .param("customerName", customerName)
+            .param("customerMessage", customerMessage))
+        .call().content();
+  }
+}
