2ac677017c0714dd5bc603b7dfebb4c1a1d189cb
Andrzej D <adrozdow@o2.pl>
2025-09-07 12:07:05 +0200

    simple logger advisor

--- ollama-simple/src/main/java/pl/atd/ollama/confi/ChatClientConfig.java
+++ ollama-simple/src/main/java/pl/atd/ollama/confi/ChatClientConfig.java
@@ -1,6 +1,7 @@
 package pl.atd.ollama.confi;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ public class ChatClientConfig {
   @Bean
   public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
     return chatClientBuilder
+        .defaultAdvisors(new SimpleLoggerAdvisor())
         .defaultUser("How can you help me?")
         .build();
   }
