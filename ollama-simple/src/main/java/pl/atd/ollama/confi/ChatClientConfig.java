5e8cde806777faa7cd78b1ac1b58a24b27308257
Andrzej D <adrozdow@o2.pl>
2025-09-07 19:07:30 +0200

    chat options

--- ollama-simple/src/main/java/pl/atd/ollama/confi/ChatClientConfig.java
+++ ollama-simple/src/main/java/pl/atd/ollama/confi/ChatClientConfig.java
@@ -2,6 +2,7 @@ package pl.atd.ollama.confi;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.atd.ollama.advisor.TokenUsageAuditAdvisor;
@@ -11,7 +12,14 @@ public class ChatClientConfig {
 
   @Bean
   public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+
     return chatClientBuilder
+        .defaultOptions(
+            ChatOptions.builder()
+                .model("llama3.2:1b")
+                .temperature(9.9)
+                .build()
+        )
         .defaultAdvisors(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor())
         .defaultUser("How can you help me?")
         .build();
