5c33d634be609445dafdee7c72d35cb25a3f116a
Andrzej D <adrozdow@o2.pl>
2025-09-14 15:38:54 +0200

    rag - simplify rag documents search with advisor

--- rag/src/main/java/pl/atd/rag/config/ChatClientConfig.java
+++ rag/src/main/java/pl/atd/rag/config/ChatClientConfig.java
@@ -1,6 +1,9 @@
 package pl.atd.rag.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +11,19 @@ import org.springframework.context.annotation.Configuration;
 public class ChatClientConfig {
 
   @Bean
-  public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-    return chatClientBuilder.build();
+  public ChatClient chatClient(ChatClient.Builder chatClientBuilder,
+      RetrievalAugmentationAdvisor retrievalAugmentationAdvisor) {
+    return chatClientBuilder.defaultAdvisors(retrievalAugmentationAdvisor)
+        .build();
+  }
+
+  @Bean
+  public RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore) {
+    return RetrievalAugmentationAdvisor.builder().documentRetriever(
+        VectorStoreDocumentRetriever.builder().vectorStore(vectorStore)
+            .topK(3)
+            .similarityThreshold(0.5)
+            .build()
+    ).build();
   }
 }
