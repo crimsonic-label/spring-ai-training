ab3524e67fa4ba7dbc48a3a9b2d3334723fcc95c
Andrzej D <adrozdow@o2.pl>
2025-09-23 21:51:37 +0200

    tool exception handling

--- tool-calling/src/main/java/pl/atd/toolcalling/HelpDeskChatClientConfig.java
+++ tool-calling/src/main/java/pl/atd/toolcalling/HelpDeskChatClientConfig.java
@@ -2,6 +2,8 @@ package pl.atd.toolcalling;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.tool.execution.DefaultToolExecutionExceptionProcessor;
+import org.springframework.ai.tool.execution.ToolExecutionExceptionProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,4 +25,11 @@ public class HelpDeskChatClientConfig {
         .defaultAdvisors(new SimpleLoggerAdvisor())
         .build();
   }
+
+  // @Bean
+  public ToolExecutionExceptionProcessor toolExecutionExceptionProcessor(){
+    // in case of tools exception, return the error directly to the client
+    return new DefaultToolExecutionExceptionProcessor(true);
+  }
+
 }
