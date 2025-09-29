7085bb3637bbd600ac51d178b3ad772d08c40363
Andrzej D. <adrozdow@o2.pl>
2025-09-29 16:24:12 +0200

    transcript audio to subtitles

--- voice-to-text/src/main/java/pl/atd/voicetotext/controller/AudioController.java
+++ voice-to-text/src/main/java/pl/atd/voicetotext/controller/AudioController.java
@@ -1,9 +1,13 @@
 package pl.atd.voicetotext.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.api.OpenAiAudioApi.TranscriptResponseFormat;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,5 +25,18 @@ public class AudioController {
   @GetMapping("/transcribe")
   public String transcribe(@Value("classpath:audio/SpringAi.mp3") Resource audioFile) {
     return openAiAudioTranscriptionModel.call(audioFile);
+  }
+  @GetMapping("/transcribe-options")
+  public String transcribeWithOptions(@Value("classpath:audio/SpringAi.mp3") Resource audioFile) {
+    return openAiAudioTranscriptionModel.call(new AudioTranscriptionPrompt(audioFile,
+        OpenAiAudioTranscriptionOptions.builder()
+            .prompt("Talking about Spring AI")
+            .language("en")
+            .temperature(0.5f)
+            // for subtitles
+            .responseFormat(TranscriptResponseFormat.VTT)
+            .build()))
+        .getResult()
+        .getOutput();
   }
 }
