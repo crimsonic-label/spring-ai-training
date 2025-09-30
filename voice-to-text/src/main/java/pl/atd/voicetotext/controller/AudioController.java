dd83f8b2584bb896d1cf51cd84907534cfc87c06
Andrzej D. <adrozdow@o2.pl>
2025-09-30 07:49:25 +0200

    text to audio

--- voice-to-text/src/main/java/pl/atd/voicetotext/controller/AudioController.java
+++ voice-to-text/src/main/java/pl/atd/voicetotext/controller/AudioController.java
@@ -1,10 +1,20 @@
 package pl.atd.voicetotext.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.api.OpenAiAudioApi.SpeechRequest.AudioResponseFormat;
+import org.springframework.ai.openai.api.OpenAiAudioApi.SpeechRequest.Voice;
 import org.springframework.ai.openai.api.OpenAiAudioApi.TranscriptResponseFormat;
+import org.springframework.ai.openai.audio.speech.SpeechModel;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +34,7 @@ import pl.atd.voicetotext.service.AudioTranscriber;
 public class AudioController {
 
   private final OpenAiAudioTranscriptionModel openAiAudioTranscriptionModel;
+  private final SpeechModel speechModel;
   private final AudioTranscriber audioTranscriber;
 
   @GetMapping("/transcribe")
@@ -34,13 +45,13 @@ public class AudioController {
   @GetMapping("/transcribe-options")
   public String transcribeWithOptions(@Value("classpath:audio/SpringAi.mp3") Resource audioFile) {
     return openAiAudioTranscriptionModel.call(new AudioTranscriptionPrompt(audioFile,
-        OpenAiAudioTranscriptionOptions.builder()
-            .prompt("Talking about Spring AI")
-            .language("en")
-            .temperature(0.5f)
-            // for subtitles
-            .responseFormat(TranscriptResponseFormat.VTT)
-            .build()))
+            OpenAiAudioTranscriptionOptions.builder()
+                .prompt("Talking about Spring AI")
+                .language("en")
+                .temperature(0.5f)
+                // for subtitles
+                .responseFormat(TranscriptResponseFormat.VTT)
+                .build()))
         .getResult()
         .getOutput();
   }
@@ -53,5 +64,26 @@ public class AudioController {
     TranscriptionRequest transcriptionRequest = new TranscriptionRequest(audioFile, context);
     TranscriptionResponse response = audioTranscriber.transcribe(transcriptionRequest);
     return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("/speech")
+  public String speech(@RequestParam("message") String message) throws IOException {
+    Path audioPath = Paths.get("output.mp3");
+    Files.write(audioPath, speechModel.call(message));
+    return "MP3 saved successfully to " + audioPath.toAbsolutePath();
+  }
+
+  @GetMapping("/speech-options")
+  public String speechWithOptions(@RequestParam("message") String message) throws IOException {
+    Path audioPath = Paths.get("output.mp3");
+    Files.write(audioPath, speechModel.call(
+        new SpeechPrompt(message,
+            OpenAiAudioSpeechOptions.builder()
+                .voice(Voice.NOVA)
+                .speed(2.0f)
+                .responseFormat(AudioResponseFormat.FLAC)
+                .build()))
+        .getResult().getOutput());
+    return "MP3 saved successfully to " + audioPath.toAbsolutePath();
   }
 }
